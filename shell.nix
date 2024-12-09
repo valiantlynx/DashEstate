b39c8a0ef77e8e8ff1d6485897da9b25c6c29a9a
valiantlynx <gormerykombo@gmail.com>
2024-12-09 17:39:10 +0100

    nix environment setup for python

--- shell.nix
+++ shell.nix
@@ -12,6 +12,9 @@ pkgs.mkShell {
     pkgs.python3Packages.pandas
     pkgs.python3Packages.numpy
     pkgs.python3Packages.matplotlib
+    pkgs.python3Packages.jupyter
+    pkgs.python3Packages.scikit-learn
+    pkgs.python3Packages.flask
     pkgs.uv
   ];
 
